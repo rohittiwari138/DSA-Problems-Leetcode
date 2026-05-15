21cc7df3e5037aad1f88b140ad3ad093bf803fe8
Rohit Kumar Tiwari <rohittiwari0403@gmail.com>
2026-05-16 00:02:58 +0530

    Time: 0 ms (100%), Space: 7.7 MB (84.96%) - LeetHub

---
+++ 3099-harshad-number/3099-harshad-number.cpp
@@ -0,0 +1,15 @@
+
+class Solution {
+public:
+    int sumOfTheDigitsOfHarshadNumber(int x) {
+        int original = x;
+        int sum = 0;
+        while(x > 0) {
+            sum += x % 10;
+            x /= 10;
+        }
+        if(original % sum == 0)
+            return sum;
+        return -1;
+    }
+};
