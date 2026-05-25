e3dc0451415328af546e958eca6e84f6aac62553
Rohit Kumar Tiwari <rohittiwari0403@gmail.com>
2026-05-26 01:44:48 +0530

    Time: 0 ms (100%), Space: 10.7 MB (93.92%) - LeetHub

---
+++ 1732-find-the-highest-altitude/1732-find-the-highest-altitude.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int largestAltitude(vector<int>& gain) {
+        int n = gain.size();
+        int altitude=0;
+        int heighest=0;
+        for(int i=0;i<n;i++){
+            altitude = altitude +gain[i];
+            heighest = max(altitude , heighest);
+        }
+        return heighest;
+    }
+};
