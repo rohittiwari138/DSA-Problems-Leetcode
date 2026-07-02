9c9299f4179bcc0e74ff7fb1cbcc73de6a6da854
Rohit Kumar Tiwari <rohittiwari0403@gmail.com>
2026-07-02 23:26:16 +0530

    Time: 0 ms (100%), Space: 9.1 MB (24%) - LeetHub

---
+++ 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    string removeOuterParentheses(string s) {
+        int depth=0;
+        string ans = "";
+        for(int i=0;i<s.size();i++){
+                if(s[i]=='('){
+                    if(depth>0)
+                    ans += s[i];
+                     depth++;
+                }
+                else{
+                    depth--;
+                    if(depth>0)
+                    ans += s[i];
+                }
+                
+            }
+         return ans;
+    }
+};
